6ea341c529536858e430ca98eb92436c19ce8170
Jean Carlos de Castilhos <jccastilhos182@gmail.com>
2023-09-12 14:42:02 -0300

    feat: adicionado rota troco

--- src/routes/defineRoutes.ts
+++ src/routes/defineRoutes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { createVeiculo } from '../controllers/veiculo/createVeiculo';
 import { listVeiculos } from '../controllers/veiculo/listVeiculos';
 import { getPalindromos } from '../controllers/palindromos/getPalindromos';
+import { getTroco } from '../controllers/troco/getTroco';
 
 const router = express.Router();
 
@@ -13,5 +14,8 @@ router.get('/veiculos', listVeiculos);
 
 // Rota para buscar palindromos
 router.get('/palindromos', getPalindromos);
+
+//Rota para calcular o troco
+router.get('/troco', getTroco)
 
 export default router;
