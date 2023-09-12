4cbabe40d24555c9408f4aaf8bc3193e21da859d
Jean Carlos de Castilhos <jccastilhos182@gmail.com>
2023-09-12 14:30:16 -0300

    feat: adicionado service getTroco

---
+++ src/service/troco/getTrocoService.ts
@@ -0,0 +1,31 @@
+import { Troco } from "../../models/trocoModel";
+
+function calcularTroco(valorCompra: number, valorEntregue: number): Troco {
+    const troco: Troco = {
+      valorCompra,
+      valorEntregue,
+      quantidadeNotas1: 0,
+      quantidadeNotas10: 0,
+      quantidadeNotas100: 0,
+    };
+  
+    let valorTroco = valorEntregue - valorCompra;
+  
+    while (valorTroco > 0) {
+      if (valorTroco >= 100) {
+        troco.quantidadeNotas100++;
+        valorTroco -= 100;
+      } else if (valorTroco >= 10) {
+        troco.quantidadeNotas10++;
+        valorTroco -= 10;
+      } else {
+        troco.quantidadeNotas1++;
+        valorTroco -= 1;
+      }
+    }
+  
+    return troco;
+  }
+  
+  export { calcularTroco };
+  
