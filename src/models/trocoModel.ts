f96f10adff35921613fbc17b89059d30ae809cda
Jean Carlos de Castilhos <jccastilhos182@gmail.com>
2023-09-12 14:23:25 -0300

    feat: adicionado model troco

---
+++ src/models/trocoModel.ts
@@ -0,0 +1,9 @@
+interface Troco {
+    valorCompra: number;
+    valorEntregue: number;
+    quantidadeNotas1: number;
+    quantidadeNotas10: number;
+    quantidadeNotas100: number;
+}
+
+export { Troco };
