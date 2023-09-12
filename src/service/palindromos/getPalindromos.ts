327329cbd43a90ad4ebe90eab563c7a83c01f9ed
Jean Carlos de Castilhos <jccastilhos182@gmail.com>
2023-09-12 13:45:01 -0300

    feat: adicionado service palindromos

---
+++ src/service/palindromos/getPalindromos.ts
@@ -0,0 +1,20 @@
+function isPalindrome(num: number): boolean {
+    const strNum = num.toString();
+    const reversedStr = strNum.split('').reverse().join('');
+    return strNum === reversedStr;
+  }
+  
+  function getPalindromos(noInicio: number, noFim: number): number[] {
+    const palindromos: number[] = [];
+  
+    for (let i = noInicio; i <= noFim; i++) {
+      if (isPalindrome(i)) {
+        palindromos.push(i);
+      }
+    }
+  
+    return palindromos;
+  }
+  
+  export { getPalindromos };
+  
